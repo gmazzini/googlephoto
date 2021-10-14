6a7b7e3f988b050e24bd764bd37c162c63b92699
Gianluca Mazzini <gianluca@mazzini.org>
2021-10-14 07:52:07 +0200

    Update p1.php

--- p1.php
+++ p1.php
@@ -1,9 +1,14 @@
 <?php 
+$album=$argv[1];
+echo "Album: $album\n";
 
-$p1="AF1QipO77HMMFpTErNkmUK5LuXMrSpDeR5O64nJOb2tx4ATc4fc-YoPSAeaXg_pxkDduiQ";
-$p2="VFhVdWpHU0p4Z2M5dzRXLVV6MndrNGhyRFdYWWNR";
+$p1[1]="AF1QipO77HMMFpTErNkmUK5LuXMrSpDeR5O64nJOb2tx4ATc4fc-YoPSAeaXg_pxkDduiQ";
+$p2[1]="VFhVdWpHU0p4Z2M5dzRXLVV6MndrNGhyRFdYWWNR";
 
-$url="https://photos.google.com/share/$p1?key=$p2";
+$p1[2]="AF1QipPb-oQKtgwg_x3cVSEWgwsJ_ddicNK2I0bSTmMTsh7CdNlTuGLy_wTrq3aOSuoSIg";
+$p2[2]="Z2F1SjV4dDJyTDFKempHcERDVWttbFI4YURhUTd3";
+
+$url="https://photos.google.com/share/$p1[$album]?key=$p2[$album]";
 $html=file_get_contents($url);
 file_put_contents("check.html",$html);
 $re='/<script nonce="[^"]+">AF_initDataCallback\(\{[^<]+, data:([^<]+)\}\);<\/script>/m';
@@ -16,7 +21,7 @@ $i=1;
 foreach($qq as $v){
 break;
   echo $i." ".$v[0]."\n";
-  $url="https://photos.google.com/share/$p1/photo/$v[0]?key=$p2";
+  $url="https://photos.google.com/share/$p1[$album]/photo/$v[0]?key=$p2[$album]";
   $html=file_get_contents($url);
   preg_match_all($re,$html,$matches,PREG_SET_ORDER,0);
   $json=str_replace(', sideChannel: {}', '',$matches[0][1]);
