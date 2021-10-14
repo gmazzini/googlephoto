f8346b137bef8a3d53dd3401498cee482620c596
Gianluca Mazzini <gianluca@mazzini.org>
2021-10-14 07:08:48 +0200

    Update p1.php

--- p1.php
+++ p1.php
@@ -17,13 +17,18 @@ foreach($qq as $v){
   echo $i." ".$v[0]."\n";
   $url="https://photos.google.com/share/$p1/photo/$v[0]?key=$p2";
   $html=file_get_contents($url);
-  file_put_contents($i.".html",$html);
+  
+  preg_match_all($re,$html,$matches,PREG_SET_ORDER,0);
+$json=str_replace(', sideChannel: {}', '', $matches[0][1]);
+$data=json_decode($json,true);
+print_r($data);
 
+  
   echo $i." ".$v[1][0]."\n";
   $myjpg=file_get_contents($v[1][0]);
   file_put_contents($i.".jpg",$myjpg);
   $i++;
-  if($i>3)break;
+  if($i>1)break;
 }
 
 ?>
