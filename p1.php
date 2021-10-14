bac99ee82c6a64f76eb8b40043aa0144c36c7595
Gianluca Mazzini <gianluca@mazzini.org>
2021-10-14 07:11:12 +0200

    Update p1.php

--- p1.php
+++ p1.php
@@ -7,7 +7,7 @@ $url="https://photos.google.com/share/$p1?key=$p2";
 $html=file_get_contents($url);
 $re='/<script nonce="[^"]+">AF_initDataCallback\(\{[^<]+, data:([^<]+)\}\);<\/script>/m';
 preg_match_all($re,$html,$matches,PREG_SET_ORDER,0);
-$json=str_replace(', sideChannel: {}', '', $matches[0][1]);
+$json=str_replace(', sideChannel: {}', '',$matches[0][1]);
 $data=json_decode($json,true);
 $qq=$data[1];
 
@@ -17,12 +17,11 @@ foreach($qq as $v){
   echo $i." ".$v[0]."\n";
   $url="https://photos.google.com/share/$p1/photo/$v[0]?key=$p2";
   $html=file_get_contents($url);
-  
   preg_match_all($re,$html,$matches,PREG_SET_ORDER,0);
-$json=str_replace(', sideChannel: {}', '', $matches[0][1]);
-$data=json_decode($json,true);
-print_r($data);
-
+  $json=str_replace(', sideChannel: {}', '',$matches[0][1]);
+  $data=json_decode($json,true);
+  $title=$data[10];
+  echo $i." ".$title."\n";
   
   echo $i." ".$v[1][0]."\n";
   $myjpg=file_get_contents($v[1][0]);
